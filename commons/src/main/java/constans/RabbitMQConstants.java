968d61c73cb55cc2f40e24f255158eb0b3f0133f
= <=>
2024-03-14 20:15:46 -0300

    Criando .jar comum entre produtores e consumidores

---
+++ commons/src/main/java/constans/RabbitMQConstants.java
@@ -0,0 +1,7 @@
+package constans;
+
+public class RabbitMQConstants {
+    public static final String EXCHANGE_MARKETPLACE    = "marketplace.direct";
+    public static final String QUEUE_PRODUCT_LOG       = "product.log";
+    public static final String ROUTING_KEY_PRODUCT_LOG = "product.log";
+}
